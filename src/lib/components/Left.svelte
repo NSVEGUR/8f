4ee7b06f4fc395593a167da248cb6c169077225a
geekvinay <geekvinay002@gmail.com>
2022-04-05 10:59:03 +0530

    Added left pane

---
+++ src/lib/components/Left.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<div
+  class="left--pane w-full h-full bg-white flex flex-col justify-start align-middle"
+>
+  <img src="./logo.svg" alt="" class="h-14 my-6" />
+  <div class="btns h-full w-full flex flex-col justify-start list-none">
+    <li class="active ml-[40px] px-[30px] py-[15px] rounded-2xl mr-6">
+      Dashboard
+    </li>
+    <li class="ml-[40px] px-[30px] py-[15px] rounded-2xl mr-6">Explore</li>
+    <li class="ml-[40px] px-[30px] py-[15px] rounded-2xl mr-6">My plans</li>
+    <li class="ml-[40px] px-[30px] py-[15px] rounded-2xl mr-6">Settings</li>
+  </div>
+</div>
+
+<style>
+  div.btns li {
+    /* background-color: #B8BED9; */
+    border-color: #b8bed9;
+    color: #6a7297;
+    @apply font-medium;
+  }
+
+  div.btns li.active {
+    background-color: #4062ff;
+    color: white;
+  }
+
+  div.btns li.active::after {
+    content: "";
+    display: block;
+    position: absolute;
+    height: 50px;
+    width: 40px;
+    background-color: wheat;
+    transform: translate(-50px, -100px);
+  }
+</style>
---
+++ src/lib/components/Middle.svelte
@@ -0,0 +1,9 @@
+<script></script>
+
+<div class="centerPane h-full w-full flex flex-col justify-start align-middle">
+  <div class="searchBar">
+    <input type="text" name="search" id="search" placeholder="Search" />
+  </div>
+</div>
+
+<style></style>
