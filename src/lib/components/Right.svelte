3af6ea50502e23b6b62663db974324d12b19fd72
geekvinay <geekvinay002@gmail.com>
2022-04-05 12:09:27 +0530

    Completed left pane and working on the right one

--- src/lib/components/Right.svelte
+++ src/lib/components/Right.svelte
@@ -4,19 +4,31 @@
 <div
   class="right--pane w-full h-full bg-white flex flex-col justify-start align-middle"
 >
-  <div class="profile flex flex-row justify-center align-middle">
-    <img src="./profile.svg" alt="profile" class="profile-photo h-14 m-6" />
-    <h2 class="">Vinay Kishore</h2>
+  <div class="profile flex h-[80px] w-full justify-start align-middle ">
+    <img
+      src="./person.png"
+      alt="profile"
+      class="profile-photo h-[45px] ml-8 my-3  rounded-[50%]"
+    />
+    <div class="wrapper-login">
+      <h2 class="mt-4 px-2 text-lg font-bold">Vinay Kishore</h2>
+      <p class="text-sm text-base px-3 text-gray-600 font-semibold">Student</p>
+    </div>
   </div>
+
+  <div class="infoBanner h-[150px] bg-[#4062ff] m-3 box-border rounded-2xl" />
 </div>
 
 <style>
-  .profile-photo {
-    border: solid #4062ff 5px;
-    border-radius: 50%;
+  img.profile-photo {
+    border: 5px solid blue;
+    @apply box-content;
   }
 
-  .profile {
-    border: solid red;
+  div.profile-photo {
+    background: url("/bg.svg");
+    background-position: left top;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 </style>
